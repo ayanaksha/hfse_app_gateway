e04725fbd0279743f808e43abe40605e27c44c7a
unknown <319809@cognizant.com>
2019-04-05 19:46:03 +0530

    first commit

---
+++ src/main/java/tatha/poc/myzuuleg/MyzuulegApplication.java
@@ -0,0 +1,33 @@
+package tatha.poc.myzuuleg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class MyzuulegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyzuulegApplication.class, args);
+	}
+	
+	@Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+
+}
